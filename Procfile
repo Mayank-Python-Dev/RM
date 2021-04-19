fab9fd9c36ba5a10b1c2ae0afbee0f5f03f2495d
mayank <Maymj149@yahoo.com>
2021-04-19 11:09:03 +0530

    deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Roshan.wsgi --log-file -
--- Roshan/settings.py
+++ Roshan/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xt04_+i$o0u0l50nyq&42v1l40b4ha&^10oxaramkszmbeu#8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roshanmotors-crm.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
