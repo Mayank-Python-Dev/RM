35a16451a416462fdfb5e79a3a789b7eaee1b1f2
mayank <Maymj149@yahoo.com>
2021-05-02 16:17:27 +0530

    updated

--- Roshan/settings.py
+++ Roshan/settings.py
@@ -155,9 +155,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
